e4b46ea230fdedc14b343bcf56fc9c6d33934ecd
momongaclub <threeforest8@gmail.com>
2020-10-13 17:43:26 +0900

    first commit

---
+++ voice_record/main.py
@@ -0,0 +1,44 @@
+import speech_recognition as sr
+import simpleaudio
+import time
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+while True:
+    print("何か話して下さい ...")
+
+    with mic as source:
+        r.adjust_for_ambient_noise(source) #雑音対策
+        audio = r.listen(source)
+
+    print ("識別しています ...")
+
+    try:
+        print(r.recognize_google(audio, language='ja-JP'))
+
+        # "ストップ" と言ったら音声認識を止める
+        if r.recognize_google(audio, language='ja-JP') == "ストップ" :
+            print("end")
+            break
+
+        if r.recognize_google(audio, language='ja-JP') == "ラップバトル" :
+            print('開始')
+            wav_obj = simpleaudio.WaveObject.from_wave_file('futta-jazz.wav')
+            play_obj = wav_obj.play()
+            time.sleep(5)
+            voice_obj = simpleaudio.WaveObject.from_wave_file('sample2_syn.wav')
+            play_voice_obj = voice_obj.play()
+            play_voice_obj.wait_done()
+            play_obj.wait_done()
+            # 音チョット大きく
+
+            # time スタート 終了後バースを生成
+
+
+    # 以下は認識できなかったときに止まらないように。
+    except sr.UnknownValueError:
+        print("could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
